c408d6d04ab100fc02604724e3bd7a513db4fb17
fffffatah <ab.fatahmn@gmail.com>
2021-06-09 23:18:32 +0600

    add otp params to ResetPassword body

--- ShantyWebAPI/Models/User/ResetPasswordModel.cs
+++ ShantyWebAPI/Models/User/ResetPasswordModel.cs
@@ -9,6 +9,8 @@ namespace ShantyWebAPI.Models.User
     public class ResetPasswordModel
     {
         [Required]
+        public string Otp { get; set; }
+        [Required]
         [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", ErrorMessage = "Password Must be 8 in Length and Must Contain Uppercase, Number and a Special Character")]
         public string NewPass { get; set; }
         [Required]
